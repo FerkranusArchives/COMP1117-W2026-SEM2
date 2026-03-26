bb76557436b9238a65c6b25f92c14020e505b75d
Emma Ferkranus <emmaferk@gmail.com>
2026-03-26 17:54:13 -0400

    Week 11 Lesson

--- Assets/Scripts/DestroySelf.cs
+++ Assets/Scripts/DestroySelf.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class DestroySelf : MonoBehaviour
 {
-    public void DestroyMyself()
+    public void DestroyMyself() //codeicide.......
     {
         Destroy(gameObject);
     }
